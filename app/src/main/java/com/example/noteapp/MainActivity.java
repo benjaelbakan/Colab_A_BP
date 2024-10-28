850e26d182a709b2ef4d76f1de55cf640a691885
SinChucrut1 <carrasco.emc2@gmail.com>
2024-10-28 10:38:18 -0300

    cambios menores

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -3,14 +3,15 @@ package com.example.noteapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     int numero;
 
     String Hola;
 
-    //holawenas
-
-    // que wea
+    // Crear un ArrayList donde guardar las notas
+    ArrayList<String> listaNotas = new ArrayList<>();
 
 
     @Override
